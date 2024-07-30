d8d5fc65a82748638b41c34e47bbf6d7c61ef80c
Daniel Black <daniel.black@sedulo.co.uk>
2024-07-30 12:09:54 +0100

    pipeline ci test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-// app/page.tsx
 import { Box, Heading } from '@chakra-ui/react';
 import { ScoreForm } from './ScoreForm';
 
 export default function Home() {
   return (
     <Box maxW="sm" mx="auto" mt="10">
-      <Heading as="h1" mb="6">Submit Your Happiness Score PLEASE</Heading>
+      <Heading as="h1" mb="6">Submit Your Happiness Score NOW</Heading>
       <ScoreForm />
     </Box>
   );
