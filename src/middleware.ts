03f87aef3fb46003704f98d4af48bfb70c7a64d8
Daniel Black <daniel.black@sedulo.co.uk>
2024-08-29 10:25:10 +0100

    Add middleware to protect pages

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import axios from "axios";
+import apiClient from "./lib/apiClient";
+
+export async function middleware(request: NextRequest) {
+  const apiToken = request.cookies.get("auth_token")?.value;
+
+  if (!apiToken) {
+    return NextResponse.redirect(
+      new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/login`, request.url)
+    );
+  }
+
+  try {
+    await apiClient.get(`/authentication/check`, {
+      headers: { Authorization: `Bearer ${apiToken}` },
+    });
+    return NextResponse.next();
+  } catch (error: any) {
+    if (
+      axios.isAxiosError(error) &&
+      (error.response?.status === 401 || error.response?.status === 403)
+    ) {
+      return NextResponse.redirect(
+        new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/login`, request.url)
+      );
+    }
+    return NextResponse.redirect(new URL("/error", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/"],
+};
