932e97abe4de41b3044782c121346c4361e399d8
Daniel Black <daniel.black@sedulo.co.uk>
2024-09-13 11:39:17 +0100

    fix redirection issue in deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,18 @@ import apiClient from "./lib/apiClient";
 
 export async function middleware(request: NextRequest) {
   const apiToken = request.cookies.get("auth_token")?.value;
+
+  // Determine if we are in development or production
+  const isDev = process.env.NODE_ENV === "development";
+
+  const loginPath = isDev
+    ? `${process.env.NEXT_PUBLIC_BASE_URL}/login`
+    : "/login";
+
   if (!apiToken) {
-    return NextResponse.redirect(
-      new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/login`, request.url)
-    );
+    return NextResponse.redirect(new URL(loginPath, request.url));
   }
+
   try {
     await apiClient.get(`/authentication/check`, {
       headers: { Authorization: `Bearer ${apiToken}` },
@@ -21,9 +28,7 @@ export async function middleware(request: NextRequest) {
       axios.isAxiosError(error) &&
       (error.response?.status === 401 || error.response?.status === 403)
     ) {
-      return NextResponse.redirect(
-        new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/login`, request.url)
-      );
+      return NextResponse.redirect(new URL(loginPath, request.url));
     }
     return NextResponse.redirect(new URL("/error", request.url));
   }
