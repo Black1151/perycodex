1f0ed4c3d9a722429515f8ab0d03b22d3d7fe0ba
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-08 18:37:41 +0100

    Added a little bit more information based on understanding where you are.

--- src/app/(admin)/customers/page.tsx
+++ src/app/(admin)/customers/page.tsx
@@ -1,7 +1,8 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
 import DataGridComponent from "@/components/agGrids/DataGridComponent";
-import { customerFields } from "@/components/agGrids/dataFields/customerFields";
+import {customerFields} from "@/components/agGrids/dataFields/customerFields";
+import {Heading, Text} from "@chakra-ui/react";
 
 // AG Grids
 export const dynamic = "force-dynamic";
@@ -9,46 +10,52 @@ export const revalidate = 0;
 export const fetchCache = "force-no-store";
 
 export default async function CustomersPage() {
-  const cookieStore = cookies();
-  const authToken = cookieStore.get("auth_token")?.value;
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("auth_token")?.value;
 
-  if (!authToken) {
-    return redirect("/login");
-  }
-
-  // Fetch customer data from the backend
-  const res = await fetch(
-    `${process.env.BE_URL}/customer/allBy?selectColumns=id,name,customerCode,isActive,uniqueId`,
-    {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+    if (!authToken) {
+        return redirect("/login");
     }
-  );
 
-  if (!res.ok) {
-    return redirect("/error");
-  }
-
-  const customers = await res.json();
-  const customerData = customers.resource;
-
-  // Check if customerData exists and has data
-  if (customerData && customerData.length > 0) {
-    return (
-      <>
-        <DataGridComponent
-          data={customerData}
-          initialFields={customerFields}
-          createNewUrl={"/customers/create"}
-        />
-      </>
+    // Fetch customer data from the backend
+    const res = await fetch(
+        `${process.env.BE_URL}/customer/allBy?selectColumns=name,uniqueId,address3,country,customerCode,numberOfEmployees,imageUrl,isActive`,
+        {
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+            },
+        }
     );
-  } else {
-    return (
-      <>
-        <h1>No Customers Found</h1>
-      </>
-    );
-  }
+
+    if (!res.ok) {
+        return redirect("/error");
+    }
+
+    const customers = await res.json();
+    const customerData = customers.resource;
+
+    // Check if customerData exists and has data
+    const customerCount = customerData ? customerData.length : 0;
+
+    // Check if customerData exists and has data
+    if (customerData && customerCount > 0) {
+        return (
+            <>
+                <Heading as="h1" size="lg" color={'white'} width={'full'} borderBottom={'2px solid white'}>
+                    CUSTOMERS <Text as="sup" size={'sm'}>{customerCount}</Text>
+                </Heading>
+                <DataGridComponent
+                    data={customerData}
+                    initialFields={customerFields}
+                    createNewUrl={"/customers/create"}
+                />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <h1>No Customers Found</h1>
+            </>
+        );
+    }
 }
