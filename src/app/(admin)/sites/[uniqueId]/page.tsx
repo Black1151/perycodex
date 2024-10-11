d8c604a9be5f921c25541abbc9bccd2244a30a93
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-11 11:23:02 +0100

    Updated the Sites Details Page

--- src/app/(admin)/sites/[uniqueId]/page.tsx
+++ src/app/(admin)/sites/[uniqueId]/page.tsx
@@ -1,47 +1,49 @@
 import React from "react";
 import SurveyJsComponent from "@/components/surveyJs/SurveyJsComponent";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { siteJson } from "@/components/surveyJs/forms/site";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {siteJson} from "@/components/surveyJs/forms/site";
+import {SiteDetailsBanner} from "@/components/AdminDetailsBanners/SiteDetailsBanner";
 
 export default async function SitePage({
-  params,
-}: {
-  params: { uniqueId: string };
+                                           params,
+                                       }: {
+    params: { uniqueId: string };
 }) {
-  const cookieStore = cookies();
-  const authToken = cookieStore.get("auth_token")?.value;
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("auth_token")?.value;
 
-  if (!authToken) {
-    return redirect("/login");
-  }
-
-  // Fetch site data from the backend
-  const res = await fetch(
-    `${process.env.BE_URL}/site/findBy?uniqueId=${params.uniqueId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+    if (!authToken) {
+        return redirect("/login");
     }
-  );
 
-  if (!res.ok) {
-    return redirect("/error");
-  }
+    // Fetch site data from the backend
+    const res = await fetch(
+        `${process.env.BE_URL}/site/findBy?uniqueId=${params.uniqueId}`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+            },
+        }
+    );
 
-  const site = await res.json();
-  const siteData = site.resource;
+    if (!res.ok) {
+        return redirect("/error");
+    }
 
-  return (
-    <div>
-      <SurveyJsComponent
-        jsonSchema={siteJson}
-        endpoint={"/site"}
-        isNew={false}
-        dataset={siteData}
-      />
-    </div>
-  );
+    const site = await res.json();
+    const siteData = site.resource;
+
+    return (
+        <div>
+            <SiteDetailsBanner site={siteData}/>
+            <SurveyJsComponent
+                jsonSchema={siteJson}
+                endpoint={`/site/${params.uniqueId}`}
+                isNew={false}
+                dataset={siteData}
+            />
+        </div>
+    );
 }
