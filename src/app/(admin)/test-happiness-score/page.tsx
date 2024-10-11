7cf7451eb8e014c3271c7da38523eb53e922a53a
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-11 17:16:37 +0100

    Test page for happiness score

---
+++ src/app/(admin)/test-happiness-score/page.tsx
@@ -0,0 +1,31 @@
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+
+// AG Grids
+import DataGridComponent from "@/components/agGrids/DataGridComponent";
+import {groupFields} from "@/components/agGrids/dataFields/userGroupFields";
+import AdminHeader from "@/components/AdminHeader";
+import SurveyJsComponent from "@/components/surveyJs/SurveyJsComponent";
+import {happinessJson} from "@/components/surveyJs/forms/happiness";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
+
+export default async function UsersPage() {
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("auth_token")?.value;
+
+    if (!authToken) {
+        return redirect("/login");
+    }
+    
+    return (
+        <>
+            <AdminHeader headingText={'Test Happiness Score'}/>
+            <SurveyJsComponent jsonSchema={happinessJson} endpoint={'/helper'} isNew={true}/>
+        </>
+    );
+}
+
