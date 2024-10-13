1945ff895aab7e919fd858a38f168f2d8c098c0d
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-13 22:35:49 +0100

    Added a useModal hook and a Default Layout

---
+++ src/components/surveyjs-new/useModal.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+// Custom hook for modal behavior
+const useModal = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
+    return {
+        isOpen,
+        openModal,
+        closeModal,
+    };
+};
+
+export default useModal;
