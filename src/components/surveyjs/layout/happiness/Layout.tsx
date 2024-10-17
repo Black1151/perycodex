b0247e3e67857398f39ef1a3d3b1d0fe640e94aa
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-17 17:37:49 +0100

    Added the ability to show a title

--- src/components/surveyjs/layout/happiness/Layout.tsx
+++ src/components/surveyjs/layout/happiness/Layout.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Survey} from 'survey-react-ui';
-import {Flex} from "@chakra-ui/react";
+import {Flex, Heading} from "@chakra-ui/react";
 import useSurveyNavigation from "@/components/surveyjs/useSurveyNavigation";
-import {LayoutProps} from "@/components/surveyjs/SurveyProps";
+import {HappinessLayoutProps, LayoutProps} from "@/components/surveyjs/SurveyProps";
+import {LetterFlyIn} from "@/components/animations/text/LetterFlyIn";
 
 
-const HappinessLayout: React.FC<LayoutProps> = ({model, dataset}) => {
+const HappinessLayout: React.FC<HappinessLayoutProps> = ({
+                                                             model,
+                                                             dataset,
+                                                             showTitle = false
+                                                         }) => {
     const {
         currentPage,
         setCurrentPage,
@@ -22,8 +27,10 @@ const HappinessLayout: React.FC<LayoutProps> = ({model, dataset}) => {
     } = useSurveyNavigation(model, dataset);
 
     return (
-        <Flex w="full" justify="center" align="center" pt={'60px'} position="fixed" direction="column" top={0} left={0}
-              height={'100svh'} width={'100svw'}>
+        <Flex w="full" justify="center" align="center" pt={'60px'} bg={'perygonPink'} position="fixed"
+              direction="column" top={0} left={0}
+              height={'100svh'} width={'100svw'} zIndex={101}>
+            {showTitle && (<LetterFlyIn>What is your happiness score this week?</LetterFlyIn>)}
             <Survey model={model}/>
         </Flex>
     );
