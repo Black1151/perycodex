eef0ffc668b3c88cd8e728d24375586691e06e48
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-17 12:14:25 +0100

    Specifically for admin - added a way to check if a user should have access to this particular page

---
+++ src/lib/checkUserRole.ts
@@ -0,0 +1,77 @@
+import {redirect} from "next/navigation";
+
+const roleBasedRoutes: { [key: string]: string[] } = {
+    CA: [
+        "/customers",
+        "/customers/[dynamicSegment]",
+        "/customers/create",
+        "/sites",
+        "/sites/[dynamicSegment]",
+        "/sites/create",
+        "/tags",
+        "/tags/[dynamicSegment]",
+        "/tags/create",
+        "/teams",
+        "/teams/[dynamicSegment]",
+        "/teams/create",
+        "/user-groups",
+        "/user-groups/[dynamicSegment]",
+        "/user-groups/create",
+        "/users",
+        "/users/[dynamicSegment]",
+        "/users/create",
+        "/my-company",
+        "/my-profile",
+    ],
+    PA: [
+        "/customers",
+        "/customers/[dynamicSegment]",
+        "/customers/create",
+        "/sites",
+        "/sites/[dynamicSegment]",
+        "/sites/create",
+        "/tags",
+        "/tags/[dynamicSegment]",
+        "/tags/create",
+        "/teams",
+        "/teams/[dynamicSegment]",
+        "/teams/create",
+        "/user-groups",
+        "/user-groups/[dynamicSegment]",
+        "/user-groups/create",
+        "/users",
+        "/users/[dynamicSegment]",
+        "/test-happiness-score"
+    ],
+    // Add more roles and routes as needed
+};
+
+export function checkUserRole(userIdentity: { role: string }, targetPathname: string): boolean | void {
+    if (!userIdentity || !userIdentity.role) {
+        redirect("/error");
+        return;
+    }
+
+    const allowedRoutes = roleBasedRoutes[userIdentity.role];
+
+    if (!allowedRoutes) {
+        redirect("/unauthorized");
+        return;
+    }
+
+    const isAllowed = allowedRoutes.some((route) => {
+        if (!route.includes("[dynamicSegment]")) {
+            return route === targetPathname;
+        }
+
+        const routeRegex = new RegExp(`^${route.replace("[dynamicSegment]", "[^/]+")}$`);
+        return routeRegex.test(targetPathname);
+    });
+
+    if (!isAllowed) {
+        redirect("/unauthorized");
+        return;
+    }
+
+    return true;
+}
