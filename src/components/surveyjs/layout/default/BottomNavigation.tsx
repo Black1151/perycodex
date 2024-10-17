5aa94c54cf4c64b7f38f31b2237115ca633a68e4
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-17 17:38:27 +0100

    Unsure

--- src/components/surveyjs/layout/default/BottomNavigation.tsx
+++ src/components/surveyjs/layout/default/BottomNavigation.tsx
@@ -24,13 +24,17 @@ const BottomNavigation: React.FC<NavigationProps> = ({
                                                          isEditing,
                                                      }) => {
     // Responsive value for the button text; hides text on mobile
-    const buttonText = useBreakpointValue({ base: '', md: 'Previous' });
-    const nextButtonText = useBreakpointValue({ base: '', md: 'Next' });
-    const submitButtonText = useBreakpointValue({ base: '', md: 'Submit' });
+    const buttonText = useBreakpointValue({base: '', md: 'Previous'});
+    const nextButtonText = useBreakpointValue({base: '', md: 'Next'});
+    const submitButtonText = useBreakpointValue({base: '', md: 'Submit'});
 
     return (
         <Box p={4} borderRadius="lg" w="100%">
-            <Flex justify="space-between" align="center" w="100%">
+            <Flex
+                justify={pageListOptions.length <= 1 ? "flex-end" : "space-between"}
+                align="center"
+                w="100%"
+            >
                 {/* Left section: Previous and Next buttons */}
                 {pageListOptions.length > 1 && (
                     <Stack direction="row" spacing={4}>
@@ -38,15 +42,15 @@ const BottomNavigation: React.FC<NavigationProps> = ({
                             fontSize={'sm'}
                             onClick={prevPage}
                             disabled={isFirstPage}
-                            leftIcon={<ArrowBackIcon />}
+                            leftIcon={<ArrowBackIcon/>}
                             bgColor="darkGray"
                             w="full"
                             height={12}
                             border="1px solid darkGray"
                             color="white"
-                            _hover={{ color: "darkGray", backgroundColor: "white" }}
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
+                            _hover={{color: "darkGray", backgroundColor: "white"}}
+                            whileHover={{scale: 1.05}}
+                            whileTap={{scale: 0.95}}
                         >
                             {buttonText}
                         </MotionButton>
@@ -54,16 +58,16 @@ const BottomNavigation: React.FC<NavigationProps> = ({
                         <MotionButton
                             fontSize={'sm'}
                             onClick={nextPage}
-                            rightIcon={<ArrowForwardIcon />}
+                            rightIcon={<ArrowForwardIcon/>}
                             disabled={isLastPage}
                             bgColor="darkGray"
                             w="full"
                             height={12}
                             border="1px solid darkGray"
                             color="white"
-                            _hover={{ color: "darkGray", backgroundColor: "white" }}
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
+                            _hover={{color: "darkGray", backgroundColor: "white"}}
+                            whileHover={{scale: 1.05}}
+                            whileTap={{scale: 0.95}}
                         >
                             {nextButtonText}
                         </MotionButton>
@@ -76,15 +80,15 @@ const BottomNavigation: React.FC<NavigationProps> = ({
                         <MotionButton
                             fontSize={'sm'}
                             onClick={submitSurvey}
-                            leftIcon={<DoneIcon />}
+                            leftIcon={<DoneIcon/>}
                             bgColor="green"
                             w="full"
                             height={12}
                             border="1px solid lightGray"
                             color="white"
-                            _hover={{ color: "green", backgroundColor: "white" }}
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
+                            _hover={{color: "green", backgroundColor: "white"}}
+                            whileHover={{scale: 1.05}}
+                            whileTap={{scale: 0.95}}
                         >
                             {submitButtonText}
                         </MotionButton>
