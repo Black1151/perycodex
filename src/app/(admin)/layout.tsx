a082243f4b2ed617891cfb129b93d8f399b0f28e
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-17 12:15:23 +0100

    Updated to remove the duplicated code and just check to see if a user should have access or not using the new common way

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from "react";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { AdminLayout } from "@/app/(admin)/AdminLayout";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
+import {ReactNode} from "react";
+import {redirect} from "next/navigation";
+import {AdminLayout} from "@/app/(admin)/AdminLayout";
+import {getUserIdentity} from "@/lib/getUserIdentity";
 
 export interface NavBarProps {
     userFirstName: string;
@@ -19,46 +15,8 @@ interface LayoutProps {
     children: ReactNode;
 }
 
-export default async function Layout({ children }: LayoutProps) {
-    const cookieStore = cookies();
-    const uniqueId = cookieStore.get("user_uuid")?.value;
-    const authToken = cookieStore.get("auth_token")?.value;
-
-    // Log cookie values
-    console.log("Fetched cookies:");
-    console.log("user_uuid:", uniqueId);
-    console.log("auth_token:", authToken);
-
-    if (!uniqueId || !authToken) {
-        console.log("Missing uniqueId or authToken, redirecting to login...");
-        return redirect("/login");
-    }
-
-    // Fetch user data
-    let userIdentity = null;
-
-    try {
-        console.log("Fetching user identity...");
-        const identityResponse = await fetch(
-            `${process.env.BE_URL}/getView?view=vwLoggedInUserIdentity&userUniqueId=${uniqueId}&selectColumns=customerId,role,userImageUrl,firstName`,
-            {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${authToken}`,
-                },
-            }
-        );
-
-        if (!identityResponse.ok) {
-            throw new Error('Failed to fetch user identity');
-        }
-
-        userIdentity = (await identityResponse.json()).resource;
-        console.log("Fetched user identity:", userIdentity);
-    } catch (error) {
-        console.error("Error fetching user identity:", error);
-        return redirect("/error");
-    }
+export default async function Layout({children}: LayoutProps) {
+    const userIdentity = await getUserIdentity();
 
     const userProps: NavBarProps = {
         userFirstName: userIdentity?.firstName,
