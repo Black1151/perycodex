9262ffd70ed784bbe5202282cff68ffb6bd270fc
Daniel Black <daniel.black@sedulo.co.uk>
2024-10-18 16:43:44 +0100

    fix footer

--- src/app/PerygonMainClient.tsx
+++ src/app/PerygonMainClient.tsx
@@ -12,6 +12,8 @@ import {
   ModalHeader,
   ModalBody,
   Text,
+  Flex,
+  VStack,
 } from "@chakra-ui/react";
 import { NavBar, NavBarProps } from "./NavBar";
 import { Footer } from "@/components/layout/Footer";
@@ -59,10 +61,21 @@ export const PerygonMainClient: React.FC<PerygonMainClientProps> = ({
   };
 
   return (
-    <Box minHeight="100vh" width="100%" overflow="hidden">
-      <NavBar {...navbarProps} />
-      {!showNoToolsModal && <CarouselDisplay carouselItems={carouselItems} />}
-      <Footer />
+    <Box width="100%" height="100vh">
+      <VStack
+        bgColor="red"
+        flex={1}
+        // mt={61}
+        minHeight="100%"
+        width="100%"
+        gap={0}
+        justifyContent="space-between"
+      >
+        <NavBar {...navbarProps} />
+        {!showNoToolsModal && <CarouselDisplay carouselItems={carouselItems} />}
+
+        <Footer />
+      </VStack>
       <Modal
         isOpen={showNoToolsModal}
         onClose={() => {}}
