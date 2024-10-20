f9cbdbc054ec8a1fca6318530bdf1f6da27ce84e
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-20 11:07:44 +0100

    Stopping the hydration error with <div> and <p> Removed console error with burger-case vs camelCase

--- src/components/animations/text/LetterFlyIn.tsx
+++ src/components/animations/text/LetterFlyIn.tsx
@@ -1,52 +1,54 @@
 "use client";
 
-import { Text, Box, BoxProps } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import { FC, ReactNode } from "react";
+import {Text, Box, BoxProps} from "@chakra-ui/react";
+import {motion} from "framer-motion";
+import {FC, ReactNode} from "react";
 
 const MotionBox = motion(Box);
 
 interface LetterFlyInProps extends BoxProps {
-  children: ReactNode;
-  delay?: number;
-  fontSize?: number;
-  duration?: number;
+    children: ReactNode;
+    delay?: number;
+    fontSize?: number;
+    duration?: number;
 }
 
 export const LetterFlyIn: FC<LetterFlyInProps> = ({
-  children,
-  delay = 0,
-  fontSize = 81,
-  duration = 0.1,
-  ...rest
-}) => {
-  const text = children as string;
-  const letters = text.split("");
+                                                      children,
+                                                      delay = 0,
+                                                      fontSize = 81,
+                                                      duration = 0.1,
+                                                      ...rest
+                                                  }) => {
+    const text = children as string;
+    const letters = text.split("");
 
-  return (
-    <Text
-      display="inline-block"
-      color="white"
-      fontFamily="bonfire"
-      whiteSpace="nowrap"
-      fontSize={fontSize}
-      lineHeight={1}
-      {...rest}
-    >
-      {letters.map((letter, index) => (
-        <MotionBox
-          key={index}
-          display="inline-block"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: duration,
-            delay: index * 0.1 + delay,
-          }}
+    return (
+        <Text
+            as={'span'}
+            display="inline-block"
+            color="white"
+            fontFamily="bonfire"
+            whiteSpace="nowrap"
+            fontSize={fontSize}
+            lineHeight={1}
+            {...rest}
         >
-          {letter === " " ? "\u00A0" : letter}
-        </MotionBox>
-      ))}
-    </Text>
-  );
+            {letters.map((letter, index) => (
+                <MotionBox
+                    as={'span'}
+                    key={index}
+                    display="inline-block"
+                    initial={{opacity: 0, y: -50}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{
+                        duration: duration,
+                        delay: index * 0.1 + delay,
+                    }}
+                >
+                    {letter === " " ? "\u00A0" : letter}
+                </MotionBox>
+            ))}
+        </Text>
+    );
 };
--- src/theme/scrollBarThemes.ts
+++ src/theme/scrollBarThemes.ts
@@ -9,13 +9,13 @@ export const scrollBarThemes = {
         backgroundColor: "var(--chakra-colors-gray-200)",
     },
     "::-webkit-scrollbar-track": {
-        "-webkit-box-shadow": "inset 0 0 6px rgba(0, 0, 0, 0.3)",
+        WebkitBoxShadow: "inset 0 0 6px rgba(0, 0, 0, 0.3)",
         borderRadius: "6px",
         backgroundColor: "var(--chakra-colors-gray-200)",
     },
     "::-webkit-scrollbar-thumb": {
         borderRadius: "6px",
-        "-webkit-box-shadow": "inset 0 0 6px rgba(0, 0, 0, 0.3)",
+        WebkitBoxShadow: "inset 0 0 6px rgba(0, 0, 0, 0.3)",
         backgroundColor: "var(--chakra-colors-perygonPink)",
     },
     "::-webkit-scrollbar-thumb:hover": {
