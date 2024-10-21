08b238b0c0acff13c97afef985086da1e2030916
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-21 14:41:19 +0100

    To make a view instead of the call as need to know where no domains

--- src/components/surveyjs/forms/inviteUser.ts
+++ src/components/surveyjs/forms/inviteUser.ts
@@ -4,13 +4,14 @@ export const inviteUserJson = {
             name: "invite-user-details",
             title: "Invite User",
             elements: [
+                // TODO: Change this to use view that checks if customer has a domain
                 {
                     type: "dropdown",
                     name: "customerId",
                     isRequired: true,
                     title: "Customer",
                     choicesByUrl: {
-                        url: `${process.env.NEXT_PUBLIC_BASE_URL}api/customer/allBy?parentId=null`,
+                        url: `${process.env.NEXT_PUBLIC_BASE_URL}api/customer/allBy?parentId=null&isActive=true`,
                         path: "resource",
                         valueName: "id",
                         titleName: "name"
