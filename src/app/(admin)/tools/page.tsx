426f642c39a915d30f3439a573ab3138ed703aae
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-23 20:14:07 +0100

    Updated tool get to match BE url

--- src/app/(admin)/tools/page.tsx
+++ src/app/(admin)/tools/page.tsx
@@ -10,7 +10,7 @@ export default async function ToolsPage() {
     const userIdentity = await getUserIdentity();
     checkUserRole(userIdentity, "/tools");
 
-    let url = '/tool/allConfigsBy';
+    let url = '/toolConfig/allBy';
     let headerTitle = 'Tools';
 
     const res = await apiClient(url, { cache: "no-store" });
