03743f98c743b1bb284740a18acf8c8314637e47
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-10-29 10:41:09 +0000

    Added new routes for Survey JS and AG Grids

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,90 +1,89 @@
-import React, { ReactNode } from "react";
-import { Footer } from "@/components/layout/Footer";
-import { PerygonContainer } from "@/components/layout/PerygonContainer";
-import { NavBar } from "../NavBar";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import React, {ReactNode} from "react";
+import {Footer} from "@/components/layout/Footer";
+import {PerygonContainer} from "@/components/layout/PerygonContainer";
+import {NavBar} from "../NavBar";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
 import SiteProviders from "./SiteProviders";
-import { UserContextProps } from "@/context/AdminUserContext";
 
 interface NavBarProps {
-  userFirstName: string;
-  userImageUrl: string;
-  userRole: string;
-  userCustomerId: string;
-  logoImageUrl?: string;
+    userFirstName: string;
+    userImageUrl: string;
+    userRole: string;
+    userCustomerId: string;
+    logoImageUrl?: string;
 }
 
 export default async function MainLayout({
-  children,
-}: {
-  children: ReactNode;
+                                             children,
+                                         }: {
+    children: ReactNode;
 }) {
-  const cookieStore = cookies();
-  const authToken = cookieStore.get("auth_token")?.value;
-  const uniqueId = cookieStore.get("user_uuid")?.value;
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("auth_token")?.value;
+    const uniqueId = cookieStore.get("user_uuid")?.value;
 
-  if (!authToken) {
-    redirect("/login");
-  }
+    if (!authToken) {
+        redirect("/login");
+    }
 
-  let navBarProps = {
-    userFirstName: "",
-    userImageUrl: "",
-    userRole: "",
-    userCustomerId: "",
-  };
-
-  let userMetadata = {};
-
-  try {
-    const [fetchUserInfo, fetchUserMetadata] = await Promise.all([
-      fetch(
-        `${process.env.BE_URL}/getView?view=vwLoggedInUserIdentity&userUniqueId=${uniqueId}&selectColumns=userImageUrl,firstName,role,customerId`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-          cache: "no-store",
-        }
-      ),
-      fetch(`${process.env.BE_URL}/getUserMetadata`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${authToken}`,
-        },
-        body: JSON.stringify({ p_userUniqueId: uniqueId }),
-        cache: "no-store",
-      }),
-    ]);
-
-    const userInfoData = await fetchUserInfo.json();
-    const userMetadataData = await fetchUserMetadata.json();
-
-    navBarProps = {
-      userFirstName: userInfoData.resource.firstName,
-      userImageUrl: userInfoData.resource.userImageUrl,
-      userRole: userInfoData.resource.role,
-      userCustomerId: userInfoData.resource.customerId,
+    let navBarProps = {
+        userFirstName: "",
+        userImageUrl: "",
+        userRole: "",
+        userCustomerId: "",
     };
 
-    userMetadata = userMetadataData.resource;
-  } catch (error: any) {
-    console.error("Error details:", error);
-    redirect("/error");
-  }
+    let userMetadata = {};
 
-  ////// WE NEED TO LOOK AT ADDING THE NAVBAR PROPS TO THE METADATA FETCH AND GIVING TO NAVBAR VIA PROVIDER - This may solve the stubborn user caching bug
+    try {
+        const [fetchUserInfo, fetchUserMetadata] = await Promise.all([
+            fetch(
+                `${process.env.BE_URL}/getView?view=vwLoggedInUserIdentity&userUniqueId=${uniqueId}&selectColumns=userImageUrl,firstName,role,customerId`,
+                {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                    cache: "no-store",
+                }
+            ),
+            fetch(`${process.env.BE_URL}/getUserMetadata`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${authToken}`,
+                },
+                body: JSON.stringify({p_userUniqueId: uniqueId}),
+                cache: "no-store",
+            }),
+        ]);
 
-  return (
-    <SiteProviders userMetadata={userMetadata as UserContextProps}>
-      <PerygonContainer>
-        <NavBar {...(navBarProps as NavBarProps)} />
-        {children}
-      </PerygonContainer>
-      <Footer />
-    </SiteProviders>
-  );
+        const userInfoData = await fetchUserInfo.json();
+        const userMetadataData = await fetchUserMetadata.json();
+
+        navBarProps = {
+            userFirstName: userInfoData.resource.firstName,
+            userImageUrl: userInfoData.resource.userImageUrl,
+            userRole: userInfoData.resource.role,
+            userCustomerId: userInfoData.resource.customerId,
+        };
+
+        userMetadata = userMetadataData.resource;
+    } catch (error: any) {
+        console.error("Error details:", error);
+        redirect("/error");
+    }
+
+    ////// WE NEED TO LOOK AT ADDING THE NAVBAR PROPS TO THE METADATA FETCH AND GIVING TO NAVBAR VIA PROVIDER - This may solve the stubborn user caching bug
+
+    return (
+        <SiteProviders>
+            <PerygonContainer>
+                <NavBar/>
+                {children}
+            </PerygonContainer>
+            <Footer/>
+        </SiteProviders>
+    );
 }
