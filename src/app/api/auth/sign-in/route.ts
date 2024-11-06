ad2911d13a53e64df1a9400299d44e76afbb6256
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-11-06 17:13:23 +0000

    Added the isProfileRegistered on the return so it can go the without having gone to '/'

--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -1,61 +1,58 @@
-    import {NextRequest, NextResponse} from "next/server";
-    import apiClient from "@/lib/apiClient";
+import {NextRequest, NextResponse} from "next/server";
+import apiClient from "@/lib/apiClient";
 
-    export async function POST(req: NextRequest) {
-        const {email, password} = await req.json();
+export async function POST(req: NextRequest) {
+    const {email, password} = await req.json();
 
-        try {
-            const response = await apiClient("/authentication/login", {
-                method: "POST",
-                body: JSON.stringify({
-                    email,
-                    password,
-                }),
-            });
+    try {
+        const response = await apiClient("/authentication/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
 
-            const data = await response.json();
+        const data = await response.json();
 
-            if (!response.ok || response.status !== 200) {
-                const errorMessage = data?.error || "Invalid login credentials";
-                return NextResponse.json(
-                    {error: errorMessage},
-                    {status: response.status}
-                );
-            }
-
-            const {token, UUID, role} = data.resource;
-
-            let redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`;
-
-            if (role === 'PA') {
-                redirectUrl += '/customers';
-            }
-
-
-            const res = NextResponse.json({redirectUrl});
-
-            res.cookies.set("auth_token", token, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
-            });
-
-            res.cookies.set("user_uuid", UUID, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
-            });
-
-            // res.cookies.set("user_role", role, {
-            //     httpOnly: true,
-            //     secure: process.env.NODE_ENV === "production",
-            //     sameSite: "strict",
-            // });
-
-            return res;
-        } catch (error: any) {
-            console.error(error);
-            const errorMessage = error.message || "An error occurred during login";
-            return NextResponse.json({error: errorMessage}, {status: 500});
+        if (!response.ok || response.status !== 200) {
+            const errorMessage = data?.error || "Invalid login credentials";
+            return NextResponse.json(
+                {error: errorMessage},
+                {status: response.status}
+            );
         }
+
+        const {token, UUID, role, isProfileRegistered} = data.resource;
+
+        let redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+        if (!isProfileRegistered) {
+            redirectUrl += '/profile-setup';
+        } else if (role === 'PA') {
+            redirectUrl += '/customers';
+        }
+
+
+        const res = NextResponse.json({redirectUrl});
+
+        res.cookies.set("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+
+        res.cookies.set("user_uuid", UUID, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+
+
+        return res;
+    } catch (error: any) {
+        console.error(error);
+        const errorMessage = error.message || "An error occurred during login";
+        return NextResponse.json({error: errorMessage}, {status: 500});
     }
+}
