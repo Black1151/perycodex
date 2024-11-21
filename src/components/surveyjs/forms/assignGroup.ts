f244e0e9ffc17612e25f01104180be19fd15261d
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-11-21 11:59:46 +0000

    Made the first dropdown read only for now, and replace select with Platform as placeholder

--- src/components/surveyjs/forms/assignGroup.ts
+++ src/components/surveyjs/forms/assignGroup.ts
@@ -8,6 +8,8 @@ export const assignGroupJson = {
           type: "dropdown",
           name: "fromCustomerId",
           visibleIf: "{pgv_currentUser.role} = 'PA'",
+          readOnly: true,
+          placeholder: "Platform",
           title: "Customer",
           choicesByUrl: {
             url: `/api/customer/allBy?parentId=null`,
