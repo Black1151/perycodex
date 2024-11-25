d687cf3ca124bf99df90e1270fd5adf602b52d21
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-11-25 18:37:33 +0000

    Added action based logic to the main page

--- src/app/(site)/(apps)/happiness-score/page.tsx
+++ src/app/(site)/(apps)/happiness-score/page.tsx
@@ -1,6 +1,15 @@
 import MainPageServer from "@/components/scaffold/pages/MainPageServer";
 import HappinessScoreClientInner from "./HappinessScoreClientInner";
 import WorkflowEngine from "@/app/(site)/(apps)/WorkflowEngine";
+import { Box } from "@chakra-ui/react";
+import apiClient from "@/lib/apiClient";
+import { redirect } from "next/navigation";
+
+interface WorkflowInstanceResponse {
+  resource: {
+    new_wfinstid: string;
+  };
+}
 
 export default async function Home({
   searchParams,
@@ -9,15 +18,36 @@ export default async function Home({
 }) {
   const toolId = searchParams.toolId as string;
   const workflowId = searchParams.wfId as string;
+  const action = searchParams.a as string;
 
-  return (
-    <WorkflowEngine toolId={toolId}>
-      <MainPageServer
-        searchParams={searchParams}
-        toolId={toolId}
-        workflowId={workflowId}
-        ClientInnerComponent={HappinessScoreClientInner}
-      />
-    </WorkflowEngine>
-  );
+  if (action && parseInt(action) === 1) {
+    const response = await apiClient(`/startWorkflow`, {
+      method: "POST",
+      body: JSON.stringify({
+        p_wfid: workflowId,
+        p_toolid: toolId,
+      }),
+    });
+
+    if (response.ok) {
+      const responseData: WorkflowInstanceResponse = await response.json();
+      console.log("Response data", responseData);
+      if (responseData?.resource.new_wfinstid) {
+        return redirect(
+          `happiness-score/workflow/${responseData.resource.new_wfinstid}`,
+        );
+      }
+    }
+  } else {
+    return (
+      <WorkflowEngine toolId={toolId} workflowId={workflowId}>
+        <MainPageServer
+          searchParams={searchParams}
+          toolId={toolId}
+          workflowId={workflowId}
+          ClientInnerComponent={HappinessScoreClientInner}
+        />
+      </WorkflowEngine>
+    );
+  }
 }
