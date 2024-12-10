0fccdfedfdf775aa1be81c89c8ba2a0a60ad4a44
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2024-12-10 13:10:31 +0000

    Removed pinching fix as messing with the whole layout
    Signed-off-by: Oliver Hannam <oliver.hannam@sedulo.co.uk>

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -8,8 +8,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   // fix apple iOS zooming on focusout
   useEffect(() => {
     document.documentElement.setAttribute("data-color-mode", "light");
-    document.body.style.transform = "scale(1)";
-    document.body.style.transformOrigin = "0 0";
+    // TODO: Investigate as this ruins the whole layout
+    // document.body.style.transform = "scale(1)";
+    // document.body.style.transformOrigin = "0 0";
     // if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
     //   document.body.style.transform = "scale(1)";
     //   document.body.style.transformOrigin = "0 0";
