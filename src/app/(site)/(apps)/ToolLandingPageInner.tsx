362c69ed4808cc48aba50032594d3fedcde1993d
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2025-01-09 17:17:47 +0000

    Refactored layout and some resuable pieces of code based on the eNPS needing them from the happiness-score pages

---
+++ src/app/(site)/(apps)/ToolLandingPageInner.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
+
+interface ToolLandingPageProps {
+    redirectUrl?: string | null;
+    splashScreen?: ReactNode;
+}
+
+export const ToolLandingPage = ({
+                                    redirectUrl,
+                                    splashScreen,
+                                }: ToolLandingPageProps) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (redirectUrl) {
+            if (!splashScreen) {
+                // Immediately redirect if no splash screen is provided
+                router.push(redirectUrl);
+                return;
+            }
+
+            // Set a timer for redirection if splash screen is provided
+            const timer = setTimeout(() => {
+                router.push(redirectUrl);
+            }, 5000);
+
+            return () => clearTimeout(timer); // Cleanup timer on unmount
+        }
+    }, [redirectUrl, splashScreen, router]);
+
+    // Render the splash screen if provided, otherwise render nothing
+    return splashScreen ? <>{splashScreen}</> : null;
+};
