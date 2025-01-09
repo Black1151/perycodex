3b9efe4466984603bbdee3769a0ef9fafa56a0b5
Oliver Hannam <oliver.hannam@sedulo.co.uk>
2025-01-09 16:56:19 +0000

    enps dashboard page

---
+++ src/app/(site)/(apps)/enps/dashboard/basic-dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from "@chakra-ui/react";
+
+const Dashboard = () => {
+    return (
+        <Text>This is the eNPS Dashboard</Text>
+    );
+};
+
+export default Dashboard;
