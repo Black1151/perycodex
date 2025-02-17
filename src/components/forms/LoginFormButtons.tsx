737405230757a0eedd196c83cef7588421ac7ee7
daniel.pieczonka@sedulo.co.uk <daniel.pieczonka@sedulo.co.uk>
2025-02-17 16:21:28 +0000

    Added apple icon to sso button.

--- src/components/forms/LoginFormButtons.tsx
+++ src/components/forms/LoginFormButtons.tsx
@@ -75,6 +75,11 @@ const LoginFormButtons: React.FC<LoginFormButtonsProps> = ({loading, handleButto
             >
                 <Flex align="center" justify={showText ? "flex-start" : "center"} gap={showText ? 4 : 0}
                       width="100%">
+                    <Image
+                        src="https://perygonblob.blob.core.windows.net/public/apple.png?sp=r&st=2024-10-29T11:53:27Z&se=2030-11-01T19:53:27Z&sv=2022-11-02&sr=c&sig=6el1LfIDyAeUG4tDxdrAm9t%2FLl8tg0Mysfc9lrB1g5Q%3D"
+                        alt="Google"
+                        boxSize={6}
+                    />
                     <Text>Continue with Apple</Text>
                 </Flex>
             </Button>
