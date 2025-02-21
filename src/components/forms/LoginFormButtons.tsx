12d6f61034f6878a6ae58e7713037262061bc8ba
daniel.pieczonka@sedulo.co.uk <daniel.pieczonka@sedulo.co.uk>
2025-02-21 13:26:31 +0000

    Disabled google and M$ sso on apple devices.

--- src/components/forms/LoginFormButtons.tsx
+++ src/components/forms/LoginFormButtons.tsx
@@ -33,6 +33,7 @@ const LoginFormButtons: React.FC<LoginFormButtonsProps> = ({loading, handleButto
                     <Box borderBottom="1px solid lightGray" width="100px"/>
                 </HStack>
         <Flex justify="space-around" gap={{base: 6, md: 4}} flexDirection={{base: "row", md: "column"}}>
+            {!isAppleDevice && (
             <Button
                 px={6}
                 height={12}
@@ -52,26 +53,29 @@ const LoginFormButtons: React.FC<LoginFormButtonsProps> = ({loading, handleButto
                     {showText && <Text>Continue with Microsoft</Text>}
                 </Flex>
             </Button>
-            <Button
-                px={6}
-                height={12}
-                isLoading={loading}
-                color="lightGray"
-                backgroundColor="white"
-                border="1px solid lightGray"
-                _hover={{color: "white", backgroundColor: "lightGray"}}
-                onClick={() => handleButtonClick("google")}
-            >
-                <Flex align="center" justify={showText ? "flex-start" : "center"} gap={showText ? 4 : 0}
-                      width="100%">
-                    <Image
-                        src="https://perygonblob.blob.core.windows.net/public/google.png?sp=r&st=2024-10-29T11:53:27Z&se=2030-11-01T19:53:27Z&sv=2022-11-02&sr=c&sig=6el1LfIDyAeUG4tDxdrAm9t%2FLl8tg0Mysfc9lrB1g5Q%3D"
-                        alt="Google"
-                        boxSize={6}
-                    />
-                    {showText && <Text>Continue with Google</Text>}
-                </Flex>
-            </Button>
+            )}
+            {!isAppleDevice && (
+                <Button
+                    px={6}
+                    height={12}
+                    isLoading={loading}
+                    color="lightGray"
+                    backgroundColor="white"
+                    border="1px solid lightGray"
+                    _hover={{color: "white", backgroundColor: "lightGray"}}
+                    onClick={() => handleButtonClick("google")}
+                >
+                    <Flex align="center" justify={showText ? "flex-start" : "center"} gap={showText ? 4 : 0}
+                          width="100%">
+                        <Image
+                            src="https://perygonblob.blob.core.windows.net/public/google.png?sp=r&st=2024-10-29T11:53:27Z&se=2030-11-01T19:53:27Z&sv=2022-11-02&sr=c&sig=6el1LfIDyAeUG4tDxdrAm9t%2FLl8tg0Mysfc9lrB1g5Q%3D"
+                            alt="Google"
+                            boxSize={6}
+                        />
+                        {showText && <Text>Continue with Google</Text>}
+                    </Flex>
+                </Button>
+            )}
             <Button
                 px={6}
                 height={12}
