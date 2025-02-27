4a4b73c100741907657e2bc02d801617bc1f9bd5
daniel.pieczonka@sedulo.co.uk <daniel.pieczonka@sedulo.co.uk>
2025-02-27 13:01:55 +0000

    Added logic for api side on the frontend to perform a linking call.

---
+++ src/app/api/auth/link-apple-login/route.ts
@@ -0,0 +1,80 @@
+import {NextRequest, NextResponse} from "next/server";
+import apiClient from "@/lib/apiClient";
+
+interface apiBodyType {
+    email?: string;
+    loginType?: string;
+    password?: string;
+    sub?: string;
+}
+
+export async function POST(req: NextRequest) {
+    const {email, loginType, password, sub} = await req.json();
+
+    const {searchParams} = req.nextUrl;
+
+    // Extract `searchParams` from the request URL
+    const secureLink = req.nextUrl.searchParams.get("l");
+
+    try {
+        let apiBody: apiBodyType = {};
+
+        apiBody = {
+            email: email,
+            loginType: loginType,
+            password: password,
+            sub: sub,
+        }
+
+        const response = await apiClient("/authentication/login", {
+            method: "POST",
+            body: JSON.stringify(apiBody),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok || response.status !== 200) {
+            const errorMessage = data?.error || "Invalid login credentials";
+            return NextResponse.json(
+                {error: errorMessage},
+                {status: response.status},
+            );
+        }
+
+        if (data.resource.sub) {
+            return NextResponse.json({sub: data.resource.sub});
+        } else {
+            const {token, UUID, role, isProfileRegistered} = data.resource;
+
+            let redirectUrl = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+            if (!isProfileRegistered && role !== 'EU') {
+                redirectUrl += "/profile-setup";
+            } else if (role === "PA") {
+                redirectUrl += "/customers";
+            } else if (secureLink) {
+                redirectUrl += `/link?l=${secureLink}`;
+            }
+
+            const res = NextResponse.json({redirectUrl});
+
+            res.cookies.set("auth_token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            });
+
+            res.cookies.set("user_uuid", UUID, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            });
+
+            return res;
+        }
+    } catch (error: any) {
+        console.error(error);
+        const errorMessage = error.message || "An error occurred during login";
+        return NextResponse.json({error: errorMessage}, {status: 500});
+    }
+}
