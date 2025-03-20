3097dd86978e8e8921f145a1aa9c9aacad6ff466
Black1151 <danblack9988@gmail.com>
2025-03-20 12:15:20 +0000

    refactor theming for DRY

---
+++ src/theme/themes/baseTheme.ts
@@ -0,0 +1,193 @@
+// baseTheme.ts
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+import { scrollBarThemes } from "@/theme/scrollBarThemes";
+import { agChartStyles } from "@/theme/agChartStyles";
+import { agGridStyles } from "../agGridStyles";
+
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+
+export const baseTheme = extendTheme({
+  config,
+
+  colors: {
+    primary: "blue",          
+    secondary: "#fff",        
+
+    primaryButton: "#fff",    
+    perygonBlueTransparent: "rgba(13, 0, 61, 0.85)",
+    seduloRed: "#B4213D",
+    yellow: "#EFC718",
+    lightGreen: "#92C01F",
+    seduloGreen: "#008000",
+    darkGray: "#4A4A4A",      
+
+    happinessScale: {
+      1: "#b22200",
+      2: "#e92300",
+      3: "#e74e1c",
+      4: "#e74e1c",
+      5: "#f8b133",
+      6: "#fbd238",
+      7: "#aeea92",
+      8: "#83e05c",
+      9: "#00bf00",
+      10: "#00bf00",
+    },
+  },
+
+
+  fonts: {
+    heading: "Metropolis, sans-serif",
+    body: "Metropolis, sans-serif",
+    metropolis: "Metropolis, sans-serif",
+    bonfire: "Bonfire, sans-serif",
+  },
+
+styles: {
+    global: (_props: { colorMode: "light" | "dark" }) => ({
+      ...agGridStyles,
+      ...scrollBarThemes,
+      ...agChartStyles,
+      "html, body": {
+        backgroundColor: "#ffffff",
+        color: "#000000",
+      },
+    }),
+  },
+
+  components: {
+    Button: {
+      variants: {
+        primary: {
+          mt: 5,
+          w: "full",
+          height: 12,
+          backgroundColor: "primary",
+          color: "white",
+          _hover: {
+            color: "primary",
+            border: "1px solid",
+            borderColor: "primary",
+            backgroundColor: "white",
+            _disabled: {
+              color: "white",
+              border: "none",
+              backgroundColor: "primary",
+            },
+          },
+        },
+
+        agPrimary: {
+          bg: "transparent",
+          color: "white",
+          border: "1px solid rgba(255, 255, 255, 0.2)",
+          _hover: {
+            color: "primary",
+            bg: "white",
+            boxShadow: "md",
+            _disabled: {
+              color: "white",
+              bg: "transparent",
+              boxShadow: "none",
+            },
+          },
+          _focus: {
+            boxShadow: "outline",
+          },
+        },
+
+        agPrimaryLight: {
+          bg: "transparent",
+          color: "primary",
+          border: "1px solid var(--chakra-colors-primary)",
+          _hover: {
+            color: "white",
+            bg: "primary",
+            boxShadow: "md",
+            _disabled: {
+              color: "white",
+              bg: "transparent",
+              boxShadow: "none",
+            },
+          },
+          _focus: {
+            boxShadow: "outline",
+          },
+        },
+
+        darkGray: {
+          bgColor: "darkGray",
+          border: "1px solid",
+          borderColor: "darkGray",
+          color: "white",
+          _hover: {
+            color: "darkGray",
+            backgroundColor: "white",
+            _disabled: {
+              color: "white",
+              backgroundColor: "darkGray",
+            },
+          },
+        },
+
+        green: {
+          bgColor: "seduloGreen",
+          border: "1px solid",
+          borderColor: "seduloGreen",
+          color: "white",
+          _hover: {
+            color: "green",
+            backgroundColor: "white",
+            _disabled: {
+              color: "white",
+              backgroundColor: "seduloGreen",
+            },
+          },
+        },
+
+        red: {
+          bgColor: "seduloRed",
+          border: "1px solid",
+          borderColor: "seduloRed",
+          color: "white",
+          _hover: {
+            color: "seduloRed",
+            backgroundColor: "white",
+            _disabled: {
+              color: "white",
+              backgroundColor: "seduloRed",
+            },
+          },
+        },
+      },
+    },
+
+    Switch: {
+      variants: {
+        primary: {
+          track: {
+            _checked: {
+              bg: "lightGreen",
+            },
+            bg: "#ccc",
+          },
+          thumb: {
+            bg: "white",
+          },
+        },
+      },
+    },
+  },
+
+    gradients: {
+    primaryGradient: "linear(to-br, secondary 60%, primary 100%)",
+    secondaryGradient: `linear-gradient(to bottom right, rgba(255, 0, 0, 0.6), rgba(255, 192, 203, 0.6))`,
+    secondaryGradientTransparent: `linear-gradient(to bottom right, rgba(255, 0, 0, 0.3), rgba(255, 192, 203, 0.3))`,
+  },
+});
--- src/theme/themes/perygon/perygonThemeDark/perygonThemeDark.ts
+++ src/theme/themes/perygon/perygonThemeDark/perygonThemeDark.ts
@@ -1,183 +1,52 @@
-import { color, extendTheme, type ThemeConfig } from "@chakra-ui/react";
-import { agGridStyles } from "../../../agGridStyles";
-import { scrollBarThemes } from "@/theme/scrollBarThemes";
-import { agChartStyles } from "@/theme/agChartStyles";
+// perygonDarkTheme.ts
+import { extendTheme } from "@chakra-ui/react";
 import { colorPalette } from "./colorPalette";
+import { baseTheme } from "../../baseTheme";
 
-const config: ThemeConfig = {
-  initialColorMode: "light",
-  useSystemColorMode: false,
-};
 
-export const perygonThemeDark = extendTheme({
-  config,
-  colors: {
-    primary: colorPalette.perygonDark,
-    secondary: colorPalette.perygonPurple,
-
-    // buttons
-    primaryButton: colorPalette.perygonPurple,
-
-    //legacy colors to be removed during refactor
-    perygonBlueTransparent: "rgba(13, 0, 61, 0.85)",
-    seduloRed: "#B4213D",
-    yellow: "#EFC718",
-    lightGreen: "#92C01F",
-    seduloGreen: "#008000",
-    happinessScale: {
-      1: "#b22200",
-      2: "#e92300",
-      3: "#e74e1c",
-      4: "#e74e1c",
-      5: "#f8b133",
-      6: "#fbd238",
-      7: "#aeea92",
-      8: "#83e05c",
-      9: "#00bf00",
-      10: "#00bf00",
+export const perygonThemeDark = extendTheme(
+  baseTheme,
+  {
+    colors: {
+      ...baseTheme.colors,
+      primary: colorPalette.perygonDark,
+      secondary: colorPalette.perygonPurple,
+      primaryButton: colorPalette.perygonPurple,
     },
-  },
-  fonts: {
-    heading: "Metropolis, sans-serif",
-    body: "Metropolis, sans-serif",
-    metropolis: "Metropolis, sans-serif",
-    bonfire: "Bonfire, sans-serif",
-  },
-  styles: {
-    global: (props: { colorMode: "light" | "dark" }) => {
-      return {
-        ...agGridStyles,
-        ...scrollBarThemes,
-        ...agChartStyles,
-        "html, body": {
-          backgroundColor: "#ffffff",
-          color: "#000000",
-        },
-      };
+
+    gradients: {
+      ...baseTheme.gradients,
+      primaryGradient: "linear(to-br, rgb(20, 26, 85) 60%, primary 100%)",
+      secondaryGradient: `linear-gradient(to bottom right, rgb(0, 6, 125), rgb(60, 51, 232))`,
+      secondaryGradientTransparent: `linear-gradient(to bottom right, rgba(0, 6, 125, 0.8), rgba(60, 51, 232,0.8))`,
     },
-  },
-  components: {
-    Button: {
-      variants: {
-        primary: {
-          mt: 5,
-          backgroundColor: colorPalette.perygonPurple,
-          w: "full",
-          height: 12,
-          color: "white",
-          border: "1px solid",
-          borderColor: "white",
-          _hover: {
-            color: colorPalette.perygonPurple,
-            border: "1px solid",
-            // borderColor: colorPalette.perygonPurple,
-            backgroundColor: "white",
-            _disabled: {
-              color: "white",
-              border: "none",
-              backgroundColor: "primary",
-            },
-          },
-        },
-        agPrimary: {
-          bg: "transparent",
-          color: "white",
-          border: "1px solid rgba(255, 255, 255, 0.2)",
-          _hover: {
-            color: "primary",
-            bg: "white",
-            boxShadow: "md",
-            _disabled: {
-              color: "white",
-              bg: "transparent",
-              boxShadow: "none",
-            },
-          },
-          _focus: {
-            boxShadow: "outline",
-          },
-        },
-        agPrimaryLight: {
-          bg: "transparent",
-          color: "primary",
-          border: "1px solid var(--chakra-colors-primary)",
-          _hover: {
+
+    components: {
+      ...baseTheme.components,
+      Button: {
+        variants: {
+          primary: {
+            mt: 5,
+            w: "full",
+            height: 12,
+            backgroundColor: "secondary",
             color: "white",
-            bg: "primary",
-            boxShadow: "md",
-            _disabled: {
-              color: "white",
-              bg: "transparent",
-              boxShadow: "none",
-            },
-          },
-          _focus: {
-            boxShadow: "outline",
-          },
-        },
-        darkGray: {
-          bgColor: "darkGray",
-          border: "1px solid",
-          borderColor: "darkGray",
-          color: "white",
-          _hover: {
-            color: "darkGray",
-            backgroundColor: "white",
-            _disabled: {
-              color: "white",
-              backgroundColor: "darkGray",
-            },
-          },
-        },
-        green: {
-          bgColor: "seduloGreen",
-          border: "1px solid",
-          borderColor: "seduloGreen",
-          color: "white",
-          _hover: {
-            color: "green",
-            backgroundColor: "white",
-            _disabled: {
-              color: "white",
-              backgroundColor: "seduloGreen",
-            },
-          },
-        },
-        red: {
-          bgColor: "secondary",
-          border: "1px solid",
-          borderColor: "secondary",
-          color: "white",
-          _hover: {
-            color: "secondary",
-            backgroundColor: "white",
-            _disabled: {
-              color: "white",
-              backgroundColor: "secondary",
+            border: "1px solid",
+            borderColor: "white",
+            _hover: {
+              color: "secondary",       
+              border: "1px solid",
+              backgroundColor: "white",
+              _disabled: {
+                color: "white",
+                border: "none",
+                backgroundColor: "primary", 
+                
+              },
             },
           },
         },
       },
     },
-    Switch: {
-      variants: {
-        primary: {
-          track: {
-            _checked: {
-              bg: "lightGreen",
-            },
-            bg: "#ccc",
-          },
-          thumb: {
-            bg: "white",
-          },
-        },
-      },
-    },
-  },
-  gradients: {
-    primaryGradient: "linear(to-br,rgb(20, 26, 85) 60%,primary 100%)",
-    secondaryGradient: `linear-gradient(to bottom right, rgb(0, 6, 125), rgb(60, 51, 232))`,
-    secondaryGradientTransparent: `linear-gradient(to bottom right, rgba(0, 6, 125, 0.8), rgba(60, 51, 232,0.8))`,
-  },
-});
+  }
+);
