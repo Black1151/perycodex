a595be558e0a3ce371cd53a1676da921418523ce
daniel.pieczonka@sedulo.co.uk <daniel.pieczonka@sedulo.co.uk>
2025-04-04 18:57:35 +0100

    updated types.

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,16 @@
-declare module "next-auth" {
+import NextAuth, { Session, JWT } from 'next-auth';
+
+declare module 'next-auth' {
   interface Session {
-    accessToken?: string; // Access token property (optional)
-    idToken?: string; // ID token property (optional)
+    accessToken?: string;  // Access token property (optional)
+    idToken?: string;      // ID token property (optional)
     accountProvider?: string; // Account provider (optional)
     providerAccountId?: string; // Provider Account Id (optional)
   }
 
   interface JWT {
-    accessToken?: string; // Access token property (optional)
-    idToken?: string; // ID token property (optional)
+    accessToken?: string;  // Access token property (optional)
+    idToken?: string;      // ID token property (optional)
     accountProvider?: string; // Account provider (optional)
     providerAccountId?: string; // Provider Account Id (optional)
   }
