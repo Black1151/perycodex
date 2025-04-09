1bbf7638fc4690a43f35425d25b8f1722e27bbca
dan bell <daniel.bell@sedulo.co.uk>
2025-04-09 12:10:43 +0100

    Added an optional height param for guage component

--- src/components/agCharts/AgGaugeComponent.tsx
+++ src/components/agCharts/AgGaugeComponent.tsx
@@ -11,29 +11,33 @@ LicenseManager.setLicenseKey(
   "Using_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-066268}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Sedulo_Limited}_is_granted_a_{Multiple_Applications}_Developer_License_for_{2}_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Charts_and_AG_Grid}_Enterprise___This_key_has_been_granted_a_Deployment_License_Add-on_for_{1}_Production_Environment___This_key_works_with_{AG_Charts_and_AG_Grid}_Enterprise_versions_released_before_{30_September_2025}____[v3]_[0102]_MTc1OTE4NjgwMDAwMA==8e565c62a9475b11e35b2c3b1f037177"
 );
 
+interface AgChartComponentProps {
+  flex: string;
+  title: string;
+  chartOptions: any;
+  noData: boolean;
+  height?: string; // Optional height prop
+}
+
 const AgChartComponent = ({
   flex,
   title,
   chartOptions,
   noData,
-}: {
-  flex: string;
-  title: string;
-  chartOptions: any;
-  noData: boolean;
-}) => {
+  height = "500px", // Fallback to default height
+}: AgChartComponentProps) => {
   return (
     <Box flex={flex} minWidth="300px">
       <Flex width="100%" justifyContent="center" align="center" mb={4}>
         <SectionHeader>{title}</SectionHeader>
       </Flex>
 
-      <PerygonCard height="500px">
+      <PerygonCard height={height}>
         {noData ? (
           <NoDataOverlayPink />
         ) : (
           <AgGauge
-            options={chartOptions as any}
+            options={chartOptions}
             style={{ width: "100%", height: "100%" }}
           />
         )}
