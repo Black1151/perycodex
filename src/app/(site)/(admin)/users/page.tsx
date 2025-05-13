a670edb0cba483e17412b893b9bbfda6ee55f886
Black1151 <danblack9988@gmail.com>
2025-05-13 10:09:57 +0100

    Reapply "removed ols debug console log"
    This reverts commit 7e883ab574652ff82b72963671646441f8dd6c69.
    
    B

--- src/app/(site)/(admin)/users/page.tsx
+++ src/app/(site)/(admin)/users/page.tsx
@@ -76,7 +76,6 @@ export default async function UsersPage({
 
   const users = await res.json();
   const userData = users.resource;
-  console.log("userData", userData);
 
   const userCount = userData.length;
 
