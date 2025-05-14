a22d69d42530fc74d366eed2729f68ebf74be120
Black1151 <danblack9988@gmail.com>
2025-05-14 09:42:18 +0100

    business score splash screen wip

--- src/app/(site)/(apps)/business-score/BusinessScoreSplashScreen.tsx
+++ src/app/(site)/(apps)/business-score/BusinessScoreSplashScreen.tsx
@@ -1,82 +1,123 @@
-import { Box, Center, Image } from "@chakra-ui/react";
-import { motion, useAnimation } from "framer-motion";
-import { useEffect } from "react";
+"use client";
 
-const MotionImage = motion(Image);
-const MotionBox = motion(Box);
+import LoadingBar from "@/components/LoadingBar/LoadingBar";
+import { Box, Center, Flex, Image, VStack } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { keyframes } from "@emotion/react";
 
-const SplashScreen: React.FC = () => {
-  const controls = useAnimation();
+// // === Keyframes ===
+// const swirlIn = keyframes`
+//   0% {
+//     transform: scale(0) rotate(0deg);
+//     opacity: 0;
+//   }
+//   100% {
+//     transform: scale(1) rotate(720deg);
+//     opacity: 1;
+//   }
+// `;
 
-  const fadeInDelay = 2.3;
-  const delayBeforeStart = 0.4;
-  const rotationDuration = 0.6;
-  const fadeOutDuration = 0.4;
+// const gaugePop = keyframes`
+//   0% {
+//     transform: scale(0);
+//     opacity: 0;
+//   }
+//   70% {
+//     transform: scale(1.15);
+//     opacity: 1;
+//   }
+//   100% {
+//     transform: scale(1);
+//     opacity: 1;
+//   }
+// `;
 
-  useEffect(() => {
-    const animateNeedle = async () => {
-      await new Promise((resolve) => setTimeout(resolve, fadeInDelay * 1000));
-      await controls.start({
-        opacity: 1,
-        transition: { duration: 0.5 },
-      });
-      await new Promise((resolve) =>
-        setTimeout(resolve, delayBeforeStart * 1000),
-      );
-      await controls.start({
-        rotate: [-90, 90],
-        transition: { duration: rotationDuration, ease: "easeInOut" },
-      });
-      await controls.start({
-        opacity: 0,
-        transition: { duration: fadeOutDuration },
-      });
-    };
-
-    animateNeedle();
-  }, [
-    controls,
-    fadeInDelay,
-    delayBeforeStart,
-    rotationDuration,
-    fadeOutDuration,
-  ]);
+// === Component ===
+export const BusinessScoreSplashScreen = () => {
+  const MotionVStack = motion(VStack);
+  const MotionImage = motion(Image);
 
   return (
     <Center flex={1}>
-      <Box position="relative" width="300px">
-        <MotionImage
-          src="/assets/splash-screens/business-score/PBSlogo.png"
-          alt="splash-screen"
-          width="300px"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{
-            opacity: [0, 1, 1, 0],
-            scale: [0.5, 1.1, 1, 1, 0.5],
-            rotate: [0, 0, 0, 360],
-          }}
-          transition={{
-            duration: 5.8,
-            times: [0, 0.2, 0.8, 1],
-            ease: "easeInOut",
-          }}
-        />
-        <MotionBox
-          style={{
-            position: "absolute",
-            left: "calc(50% - 4px)",
-            top: "calc(50%)",
-            width: "5px",
-            height: "50px",
-            backgroundColor: "red",
-            transformOrigin: "bottom center",
-          }}
-          initial={{ rotate: -90, opacity: 0 }}
-          animate={controls}
-        />
-      </Box>
+      <MotionVStack
+        initial={{ opacity: 1, scale: 1 }}
+        animate={{ opacity: 0, scale: 0.75 }}
+        transition={{ delay: 3.5, duration: 1, ease: "easeInOut" }}
+        position="relative"
+      >
+        {/* Background text */}
+        <Box
+          bgImage="url('/assets/splash-screens/business-score/business-score-logo-text.png')"
+          bgSize="contain"
+          bgRepeat="no-repeat"
+          bgPosition="center"
+          minW={["300px", "400px"]}
+          h={["120px", "160px"]}
+          //   animation={`${swirlIn} 1.2s ease-in-out`}
+        >
+          {/* Dial */}
+          <Flex
+            position="relative"
+            left="50%"
+            top={"40%"}
+            transform="translateX(-50%)"
+            // top={["4px", "8px"]}
+            w={["70px", "105px"]}
+            h={["70px", "105px"]}
+          >
+            {/* Base */}
+            <Image
+              src="/assets/splash-screens/business-score/base.png"
+              position="absolute"
+              w="100%"
+              top={0}
+              left={0}
+            />
+
+            {/* Coloured gauge */}
+            <MotionImage
+              src="/assets/splash-screens/business-score/gauge.png"
+              position="absolute"
+              w="100%"
+              top={0}
+              left={0}
+              //   animation={`${gaugePop} 0.8s ease-out 0.3s`}
+            />
+
+            {/* Rotating needle */}
+            <MotionImage
+              src="/assets/splash-screens/business-score/needle.png"
+              position="absolute"
+              w="100%"
+              top={0}
+              left={0}
+              initial={{ rotate: -120 }}
+              animate={{ rotate: [-120, 30, 0] }}
+              transition={{
+                delay: 1.2,
+                duration: 2.5,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatDelay: 0.5,
+              }}
+            />
+
+            {/* Centre disc */}
+            <MotionImage
+              src="/assets/splash-screens/business-score/centre.png"
+              position="absolute"
+              w={["50px", "70px"]}
+              top="50%"
+              left="50%"
+              style={{ x: "-50%", y: "-50%" }}
+              //   animation={`${gaugePop} 0.6s ease-out 0.5s`}
+            />
+          </Flex>
+        </Box>
+
+        {/* Progress bar beneath logo */}
+        <LoadingBar />
+      </MotionVStack>
     </Center>
   );
 };
-
-export default SplashScreen;
