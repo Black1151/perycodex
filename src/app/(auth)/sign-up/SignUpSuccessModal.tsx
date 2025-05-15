ada0bae8f338013383d0974f6348f42f30c0ce9a
Dan Bell <daniel.bell@sedulo.co.uk>
2025-05-15 15:43:37 +0100

    fix(signup): enhance success modal with confetti animation and icon

--- src/app/(auth)/sign-up/SignUpSuccessModal.tsx
+++ src/app/(auth)/sign-up/SignUpSuccessModal.tsx
@@ -9,7 +9,10 @@ import {
   ModalHeader,
   ModalOverlay,
   VStack,
+  Text,
 } from "@chakra-ui/react";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ConfettiAlt from "@/components/animations/confetti/ConfettiAlt";
 
 interface SignUpSuccessModalProps {
   isOpen: boolean;
@@ -21,22 +24,31 @@ export const SignUpSuccessModal = ({
   onClose,
 }: SignUpSuccessModalProps) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent p={5}>
-        <VStack gap={8}>
-          <ModalHeader>Registration Successful</ModalHeader>
-          <ModalBody>
-            Your account has been created successfully. Please check your email
-            for a verification link.
-          </ModalBody>
-          <ModalFooter>
-            <Button variant="primary" onClick={onClose}>
-              Go to Login
-            </Button>
-          </ModalFooter>
-        </VStack>
-      </ModalContent>
-    </Modal>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent p={4}>
+          <VStack gap={2}>
+            <VStack
+              fontSize={80}
+              color="green.500"
+            >
+              <CheckCircleOutlineIcon color="success" fontSize="inherit" />
+            </VStack>
+            <ModalHeader>Registration Successful</ModalHeader>
+            <ModalBody>
+              Your account has been created successfully. Please check your
+              email for a activation link.
+            </ModalBody>
+            <ModalFooter>
+              <Button variant="primary" onClick={onClose}>
+                Go to Login
+              </Button>
+            </ModalFooter>
+          </VStack>
+        </ModalContent>
+      </Modal>
+      <ConfettiAlt show={isOpen} />
+    </>
   );
 };
