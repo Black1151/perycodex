630ca8ee7749f05358bc9636e2a17fe3a68440e7
Black1151 <danblack9988@gmail.com>
2025-05-15 14:52:39 +0100

    recognition hub splash screen wip

--- src/app/(site)/(apps-non-standard)/big-up/page.tsx
+++ src/app/(site)/(apps-non-standard)/big-up/page.tsx
@@ -20,6 +20,7 @@ import { RecognitionList } from "./tabs/OtherTabs/RecognitionCardList";
 import { useUnread } from "@/components/contexts/UnreadRecognitionContext";
 import { hideScrollbar } from "@/utils/style/style-utils";
 import PerygonCard from "@/components/layout/PerygonCard";
+import { BigUpSplashScreen } from "./BigUpSplashScreen";
 
 export default function BigUpPage() {
   const router = useRouter();
@@ -56,6 +57,14 @@ export default function BigUpPage() {
     bigUpCategories,
     bigUpUnread,
   } = dashboardData;
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 6000);
+  }, []);
 
   const handleProfilePicClick = async (uuid: string) => {
     await fetchUserStats(uuid);
@@ -130,85 +139,90 @@ export default function BigUpPage() {
   }
 
   return (
-    <Flex
-      w="100%"
-      maxH="100%"
-      gap={2}
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      backgroundImage="url('/big-up/big-up-app-bg.webp')"
-      backgroundSize="cover"
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat"
-      pt={[74, null, null, null]}
-      pb={[4, 51, null, null, null]}
-      px={[2, null, null, 5]}
-      sx={{
-        "@media (max-width: 400px)": {
-          ...hideScrollbar,
-        },
-      }}
-    >
-      <PerygonCard
-        display="flex"
-        width="100%"
-        p={2}
-        pr={6}
-        alignItems="center"
-        bg={theme.components.bigUpStatsCard.baseStyle.elementBG}
-      >
-        <RecognitionHeader
-          headingText="Recognition Hub"
-          onAddButtonClick={() => setIsSubmitModalOpen(true)}
-        />
-      </PerygonCard>
+    <>
+      {isLoading && <BigUpSplashScreen />}
+      {!isLoading && (
+        <Flex
+          w="100%"
+          maxH="100%"
+          gap={2}
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          backgroundImage="url('/big-up/big-up-app-bg.webp')"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          pt={[74, null, null, null]}
+          pb={[4, 51, null, null, null]}
+          px={[2, null, null, 5]}
+          sx={{
+            "@media (max-width: 400px)": {
+              ...hideScrollbar,
+            },
+          }}
+        >
+          <PerygonCard
+            display="flex"
+            width="100%"
+            p={2}
+            pr={6}
+            alignItems="center"
+            bg={theme.components.bigUpStatsCard.baseStyle.elementBG}
+          >
+            <RecognitionHeader
+              headingText="Recognition Hub"
+              onAddButtonClick={() => setIsSubmitModalOpen(true)}
+            />
+          </PerygonCard>
 
-      <Grid
-        width={["100%"]}
-        gridTemplateColumns={["1fr", null, null, "1fr 2fr"]}
-        gap={5}
-      >
-        <VStack width="100%" gap={5}>
-          <BigUpMasonry {...masonryData} />
-        </VStack>
+          <Grid
+            width={["100%"]}
+            gridTemplateColumns={["1fr", null, null, "1fr 2fr"]}
+            gap={5}
+          >
+            <VStack width="100%" gap={5}>
+              <BigUpMasonry {...masonryData} />
+            </VStack>
 
-        <SpringScale delay={0} style={{ width: "100%" }}>
-          <PerygonTabs tabs={tabsData} />
-        </SpringScale>
+            <SpringScale delay={0} style={{ width: "100%" }}>
+              <PerygonTabs tabs={tabsData} />
+            </SpringScale>
 
-        <SubmitScoreModal
-          isOpen={isSubmitModalOpen}
-          onClose={() => setIsSubmitModalOpen(false)}
-          onSubmit={handleSubmitRecognition}
-          teamMembers={modalData.teamMembers}
-          categories={modalData.categories}
-        />
-      </Grid>
+            <SubmitScoreModal
+              isOpen={isSubmitModalOpen}
+              onClose={() => setIsSubmitModalOpen(false)}
+              onSubmit={handleSubmitRecognition}
+              teamMembers={modalData.teamMembers}
+              categories={modalData.categories}
+            />
+          </Grid>
 
-      <ConfettiWrapper show={showConfetti} />
+          <ConfettiWrapper show={showConfetti} />
 
-      <RecognitionSuccessModal
-        isOpen={showSuccessModal}
-        onClose={() => {
-          setShowSuccessModal(false);
-          setIsSubmitModalOpen(false);
-          setShowConfetti(false);
-        }}
-      />
+          <RecognitionSuccessModal
+            isOpen={showSuccessModal}
+            onClose={() => {
+              setShowSuccessModal(false);
+              setIsSubmitModalOpen(false);
+              setShowConfetti(false);
+            }}
+          />
 
-      <UserStatsModal
-        isOpen={isUserStatsModalOpen}
-        userStats={userStats}
-        onClose={() => setIsUserStatsModalOpen(false)}
-        handleProfilePicClick={handleLeaderboardProfilePicClick}
-      />
+          <UserStatsModal
+            isOpen={isUserStatsModalOpen}
+            userStats={userStats}
+            onClose={() => setIsUserStatsModalOpen(false)}
+            handleProfilePicClick={handleLeaderboardProfilePicClick}
+          />
 
-      <NewRecognitionModal
-        isOpen={isNewRecognitionModalOpen}
-        onClose={handleCloseNewRecognitionModal}
-        unreadRecognitionModalData={unreadRecognitionModalData}
-      />
-    </Flex>
+          <NewRecognitionModal
+            isOpen={isNewRecognitionModalOpen}
+            onClose={handleCloseNewRecognitionModal}
+            unreadRecognitionModalData={unreadRecognitionModalData}
+          />
+        </Flex>
+      )}
+    </>
   );
 }
