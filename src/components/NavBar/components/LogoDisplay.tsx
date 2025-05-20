6e925bcd1714fd98cbea43856d54a7097cb4f93a
Dan Bell <daniel.bell@sedulo.co.uk>
2025-05-20 17:03:19 +0100

    feat: started changing the logo display so we can show the trial indicator, not finished

--- src/components/NavBar/components/LogoDisplay.tsx
+++ src/components/NavBar/components/LogoDisplay.tsx
@@ -1,6 +1,6 @@
 // components/NavBar/LogoDisplay.tsx
 import React from "react";
-import { Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Badge } from "@chakra-ui/react";
 import Link from "next/link";
 
 interface LogoDisplayProps {
@@ -16,16 +16,56 @@ const LogoDisplay: React.FC<LogoDisplayProps> = ({
 }) => {
   const logoContent =
     toolLogo && toolLogo.trim() !== "" ? (
-      <Image src={toolLogo} alt="logo" height="50px" objectFit="contain" />
+      // <HStack
+      //   spacing={0}
+      //   alignItems="center"
+      //   justifyContent="center"
+      //   ml={3}
+      //   //position="relative"
+      // >
+      //   <Image src={toolLogo} alt="logo" height="50px" objectFit="contain" />
+      //   <Badge
+      //     colorScheme="green"
+      //     h={"min-content"}
+      //     // position={"absolute"}
+      //     // bottom={[0., 0]}
+      //     // left={[0, 0]}
+      //     fontSize={[10, 12]}
+      //     transform={["rotate(45deg)", "rotate(-25deg)"]}
+      //   >
+      //     TRIAL
+      //   </Badge>
+      // </HStack>
+
+              <Image src={toolLogo} alt="logo" height="50px" objectFit="contain" />
+
     ) : (
-      <Text
-        fontFamily="bonfire"
-        fontSize={[30, 40]}
-        bgClip="text"
-        color="white"
+      <HStack
+        spacing={0}
+        alignItems="center"
+        justifyContent="center"
+        //position="relative"
       >
-        Perygon
-      </Text>
+        <Text
+          fontFamily="bonfire"
+          fontSize={[30, 40]}
+          bgClip="text"
+          color="white"
+        >
+          Perygon
+        </Text>
+        <Badge
+          colorScheme="green"
+          h={"min-content"}
+          // position={"absolute"}
+          // bottom={[0, 4]}
+          // right={[0, -5]}
+          fontSize={[10, 12]}
+          transform={["rotate(45deg)", "rotate(-25deg)"]}
+        >
+          TRIAL
+        </Badge>
+      </HStack>
     );
 
   return userRole === "EU" ? (
